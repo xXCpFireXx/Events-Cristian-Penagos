a458e99dd511e24d61eb681b3d0f88ed5b67e728
xXCpFireXx <suarezalejo26@gmail.com>
2025-11-14 11:42:00 -0500

    feat: start project

---
+++ src/test/java/com/lovelace/UserStory_1/UserStory1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovelace.UserStory_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserStory1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
